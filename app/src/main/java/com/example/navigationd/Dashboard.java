d5ac44e0e18f656d1241eaf4bd0a4194006e1064
hadesshinigami <baka.amil@gmail.com>
2021-03-25 14:52:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/navigationd/Dashboard.java
@@ -0,0 +1,57 @@
+package com.example.navigationd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        MainActivity.openDrawer(drawerLayout);
+
+    }
+
+    public void ClickLogo(View view){
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+
+    public void ClickHome(View view){
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickDashboard(View view){
+        recreate();
+    }
+
+    public void ClickAboutUs(View view){
+        MainActivity.redirectActivity(this,AboutUs.class);
+    }
+
+    public void ClickLogout(View view){
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
